1f10a9b8d1b2ba9dcd1c0225b9f8b967f079b0b4
Or Bachar <or@localcoin.com>
2019-05-16 21:25:11 +0300

    remove scripts

--- scripts/cancel-pending-tx.js
+++
@@ -1,7 +0,0 @@
-const actions = require('../actions');
-
-const txHash = '0x31299e887af6dc4baa59c41269d76e370a15cd199e984764436f9eb58b17f74a';
-const privateKey = '0x';
-const gasPriceWei = 15000000000;
-
-actions.transaction.cancelPendingTx(txHash, privateKey, gasPriceWei);
