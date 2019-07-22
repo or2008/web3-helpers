cd81d0df921d78260926c07328a690693539857a
Or Bachar <or@localcoin.com>
2019-07-22 18:00:56 +0300

    export web3-helpers as class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import Web3 from 'web3';
 import * as utils from './utils';
 import * as actions from './actions';
 import * as helpers from './helpers';
+import * as services from './services';
 
-export {
-    utils,
-    actions,
-    helpers
+export class Web3Helpers {
+    public constructor(web3ProviderPath = 'ws://127.0.0.1:8546') {
+        services.web3.init(web3ProviderPath);
+    }
+
+    public getWeb3Instance(): Web3 {
+        return services.web3.getInstance();
+    }
+
+    public static utils = utils;
+    public static actions = actions;
+    public static helpers = helpers;
 }
